24d1e2675b28b3b97724e06b4eb75977657651bc
Saif uddin Babar <mdbabar4202@gmail.com>
2025-11-13 10:54:33 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 4000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Server is running...');
+});
+
+app.listen(port, () => console.log(`🚀 Server running on port ${port}`));
